a93042c75d9d8dc909dd588f35e89c6c82b593be
Simmo Saan <simmo.saan@gmail.com>
2010-12-12 11:23:15 +0200

    Fixed short namespace alias Added 'All' compilation mode
    Signed-off-by: Simmo Saan <simmo.saan@gmail.com>

--- include/wrench/wrench.hpp
+++ include/wrench/wrench.hpp
@@ -1,12 +1,14 @@
 #ifndef WRENCH_H
 #define WRENCH_H
 
-namespace wr = wrench;
-
 #include "General.hpp"
 #include "Math.hpp"
 #include "String.hpp"
 #include "Console.hpp"
 #include "Timer.hpp"
+
+    #ifndef WRENCH_NOSHORT
+    namespace wr = wrench;
+    #endif
 
 #endif // WRENCH_H
